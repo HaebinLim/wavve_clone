0d6c2434eb36ab806e1a74fd603ee09be8b0e257
HaebinLim <goguma-guma@hanmail.net>
2023-03-22 22:11:48 +0900

    wavve clone coding - footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,44 @@
 <template>
-  <footer>
+  <footer class="footer">
     <div class="inner">
-      <div>
-        <ul class="footer__links">
-          <li><a href="##">회사소개</a></li>
-          <li><a href="##">인재채용</a></li>
-          <li><a href="##">서비스 소개</a></li>
-          <li><a href="##">이용약관</a></li>
-          <li><a href="##">개인정보 처리방침</a></li>
-          <li><a href="##">고객센터</a></li>
-        </ul>
-        <div class="footer__copyright">
+      <ul class="footer__links">
+        <li><a href="##">회사소개</a></li>
+        <li><a href="##">인재채용</a></li>
+        <li><a href="##">서비스 소개</a></li>
+        <li><a href="##">이용약관</a></li>
+        <li><a href="##">개인정보 처리방침</a></li>
+        <li><a href="##">고객센터</a></li>
+      </ul>
+      <div class="footer__copyright">
+        <div class="txt">
           <span>콘텐츠웨이브 주식회사</span>
-          <span>따라 만들어 보기</span>
+          <span>대표이사 이태현</span>
+          <span>고객센터 1599-3709 (평일 09:00~18:00 / 점심시간 12:00~13:00 / 주말 및 공휴일 휴무)</span>
+        </div>
+        <div class="txt">
+          <span>사업자등록번호 220-88-38020</span>
+          <span>스팅서비스제공자 : 마이크로소프트 유한회사, 구글클라우드코리아 유한회사, 아마존웹서비시즈코리아 유한회사</span>
+        </div>
+        <div class="txt">
+          <span>통신판매업 신고번호 : 제 2021-서울영등포-0585호</span>
+          <span>통신판매업 정보 공개 : http://www.ftc.go.kr/bizCommPop.do?wrkr_no=220-88-38020</span>
+        </div>
+        <div class="txt">
           <span>서울특별시 영등포구 여의나루로 60 포스트타워 19층</span>
+          <span>전자우편주소 : helpdesk@wavve.com</span>
+        </div>
+        <div class="txt">
           <span>Copyright© 콘텐츠웨이브(주) All rights reserved.</span>
         </div>
       </div>
-      <div class="sns">
-        <button type="button">페이스북</button>
-        <button type="button">블로그</button>
+      <div class="footer__sns">
+        <button type="button" class="sns__btn sns__btn--facebook"><span class="sr-only">페이스북</span></button>
+        <button type="button" class="sns__btn sns__btn--blog"><span class="sr-only">블로그</span></button>
       </div>
     </div>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+@import '../assets/scss/components/footer';
+</style>
